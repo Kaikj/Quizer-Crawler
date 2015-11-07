1d4550dc3d7854dec40c6b8e5d8490522bfd87ed
Kaikj <bluesword246@gmail.com>
2015-11-07 23:02:00 +0800

    Make the crawler crawl in parallel

--- Crawler/src/Crawler.java
+++ Crawler/src/Crawler.java
@@ -3,25 +3,75 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.concurrent.Callable;
 
 public class Crawler {
 	private Database db;
-	private HashMap<String, LinkedList<String>> urlQueue;
+	private LinkedList<String> queue;
 
 	public static void main(String[] args) throws IOException {
-		Crawler c = new Crawler();
-		c.crawl();
+		Database db = new Database();
+		LinkedList<Thread> threadList = new LinkedList<Thread>();
+		while (true) {
+			// Remove dead threads for garbage collection
+			for (Thread t : threadList) {
+				if (!t.isAlive()) {
+					threadList.remove(t);
+				}
+			}
+
+			// Run the crawler on any new seeds
+			LinkedList<String> seedURLS = db.getSeedUrls();
+			for (String s : seedURLS) {
+				// Simple way to do it to avoid extending Thread
+				boolean contains = false;
+				for (Thread t : threadList) {
+					if (t.getName().equals(s)) {
+						contains = true;
+						break;
+					}
+				}
+
+				// Spawn a new thread to handle the seed
+				if (!contains) {
+					Parallel p = new Parallel(s);
+					Thread t = new Thread(p, s);
+					t.start();
+					threadList.add(t);
+				}
+			}
+
+			// Wait for any one thread to finish
+			// Continue on the outer loop to check
+			// for new seeds if none of the threads
+			// are done or have any error
+			for (Thread t : threadList) {
+				try {
+					t.join(3600000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
-	public Crawler() {
-		db = new Database();
-		urlQueue = new HashMap<String, LinkedList<String>>();
-		LinkedList<String> urls = db.getSeedUrls();
-		for (String s : urls) {
-			LinkedList<String> ll = new LinkedList<String>();
-			ll.add(s);
-			urlQueue.put(s, ll);
+	static class Parallel implements Runnable {
+		String base;
+		public Parallel(String url) {
+			base = url;
 		}
+
+		@Override
+		public void run() {
+			Crawler c = new Crawler(base);
+			c.crawl();
+		}
+	}
+
+	public Crawler(String base) {
+		db = new Database();
+		queue = new LinkedList<String>();
+		queue.add(base);
 	}
 
 	/**
@@ -29,69 +79,49 @@ public class Crawler {
 	 * including any heuristics is here!
 	 */
 	public void crawl() {
-		// Just keep crawling. No end unless system interrupt
-		while (true) {
-			// Roll through the queues in an attempt to be polite
-			for (Map.Entry<String, LinkedList<String>> entry : urlQueue.entrySet()) {
-				String seed = entry.getKey();
-				LinkedList<String> urlToVisit = entry.getValue();
-				// If there is nothing to crawl, restart from the
-				// seed url. There might be new seeds
-				if (urlToVisit.isEmpty()) {
-					urlToVisit.add(seed);
-					// Check for new seeds
-					LinkedList<String> urls = db.getSeedUrls();
-					for (String s: urls) {
-						if (!urlQueue.containsKey(s)) {
-							LinkedList<String> ll = new LinkedList<String>();
-							ll.add(s);
-							urlQueue.put(s, ll);
-						}
-					}
-				}
+		// Just keep crawling. No end unless queue is empty
+		while (!queue.isEmpty()) {
+			// Great! The url to crawl!
+			String url = queue.removeFirst();
+			Page page;
+			try {
+				page = new Page(url);
+				page.get();
+				addToQueue(page.getLinks(), page, queue);
 
-				// Great! The url to crawl!
-				String url = urlToVisit.removeFirst();
-				Page page;
-				try {
-					page = new Page(url);
-					page.get();
-					addToQueue(page.getLinks(), page, urlToVisit);
-
-					// We only want real content, not anything
-					// from the front page as there are mostly
-					// only headlines there
-					if (!page.getPath().equals("/")) {
-						LinkedList<String> sentences = page.getSentences();
-						for (String s : sentences) {
-							String new_s = s.trim();
-							StringTokenizer st = new StringTokenizer(new_s, " ", false);
-							// We do not want a sentence that is too short or blacklisted.
-							if (!new_s.isEmpty() && (st.countTokens() > 6)) {
-								boolean blacklist = false;
-								while(st.hasMoreTokens()){
-									String word = st.nextToken();
-									blacklist = blacklist || BlackList.isBlacklisted(word);
-								}
-								if(!blacklist){
-									addSentence(new_s, url);
-								}
+				// We only want real content, not anything
+				// from the front page as there are mostly
+				// only headlines there
+				if (!page.getPath().equals("/")) {
+					LinkedList<String> sentences = page.getSentences();
+					for (String s : sentences) {
+						String new_s = s.trim();
+						StringTokenizer st = new StringTokenizer(new_s, " ", false);
+						// We do not want a sentence that is too short or blacklisted.
+						if (!new_s.isEmpty() && (st.countTokens() > 6)) {
+							boolean blacklist = false;
+							while(st.hasMoreTokens()){
+								String word = st.nextToken();
+								blacklist = blacklist || BlackList.isBlacklisted(word);
+							}
+							if(!blacklist){
+								addSentence(new_s, url);
 							}
 						}
-
-						// We don't want the root page in the list
-						// of visited url either. Then we can visit
-						// them again for new content
-						db.insertVisitedUrl(url);
 					}
-				} catch (MalformedURLException e) {
-					// If the url is bad, just skip it
-					// and carry on. A few missing links won't hurt
-					// continue;
-				} catch (IOException e) {
-					// Parsing failed. Don't care and move on
-					// continue;
+
+					// We don't want the root page in the list
+					// of visited url either. Then we can visit
+					// them again for new content
+					db.insertVisitedUrl(url);
 				}
+			} catch (MalformedURLException e) {
+				// If the url is bad, just skip it
+				// and carry on. A few missing links won't hurt
+				// continue;
+			} catch (IOException e) {
+				// Parsing failed. Don't care and move on
+				// continue;
 			}
 		}
 	}
